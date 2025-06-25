211689a75e326e0cda093c68a55623a96ba9e0a9
Paul <paul@theapplegates.com>
2025-06-25 15:11:36 -0400

    first commit

--- .next/static/chunks/docs_app_icon_svg_mjs_31cff352._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/75ef5_next_dist_e1bf6da9._.js"
-],
-    source: "dynamic"
-});
